74ea94869557514547c69f481443890fa379dfa5
alando46 <music2177@gmail.com>
2016-05-26 07:37:36 -0700

    minor views adjustments

--- topmenu/views.py
+++ topmenu/views.py
@@ -6,11 +6,12 @@ from django.http import Http404
 
 def index(request):
 	print "index"
-	latest_listing_list = Listing.objects.order_by('-pub_date')[:5]
+	latest_listing_list = Listing.objects.order_by('-pub_date')[:6]
 	context = {'latest_listing_list': latest_listing_list,}
 	return render(request, 'topmenu/index.html', context)
 
 def detail(request, detail_id):
 	print "detail"
+
 #	detail = get_object_or_404(Listing_detail, pk=detail_id)
 #	return render(request, 'topmenu/detail.html', {'listing': listing})
