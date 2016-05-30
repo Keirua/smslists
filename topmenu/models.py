1e62a6ad6359f214250a3866b28ce52de782519f
alando46 <music2177@gmail.com>
2016-05-30 09:37:56 -0700

    added send sms functionality

--- topmenu/models.py
+++ topmenu/models.py
@@ -14,9 +14,22 @@ class Listing(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
-#class Listing_detail(models.Model):
-#	listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-#	ld_text = models.CharField(max_length=200)
-#	def __str__(self):
-#		return self.ld_text
-	
+class User_data(models.Model): # is serial id automatically pk?
+	user_id = models.AutoField(primary_key = True)
+	phone_num = models.PositiveIntegerField(max_length = 10)
+	user_carrier = models.PositiveIntegerField(max_length = 3)
+	user_jointime = models.DateTimeField() #need to read up on this
+	user_state = models.PositiveIntegerField(max_length = 2)
+	#user_loc = models.CharField() # read up on CharField parameters. this will eventually make it's own api call to map a radius
+	user_sms_quant = models.PositiveIntegerField(max_length = 4)
+	user_language = models.CharField(max_length = 20)
+
+class SMS_data(models.Model):
+	sms_id = models.AutoField(primary_key = True)
+	messageuuid = models.CharField(max_length = 40)
+	user_id = models.ForeignKey('User_data')
+	source = models.PositiveIntegerField(max_length= 11)
+	destination = models.PositiveIntegerField(max_length = 11)
+	message_content = models.CharField(max_length = 140)
+	# image_content = models.ImageField()
+	message_time = DateTimeField(default = timezone.now) #this will auto populate or do i handle in views?
--- topmenu/views.py
+++ topmenu/views.py
@@ -5,6 +5,9 @@ from .models import Listing
 from django.http import Http404
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+now = datetime.now()
+
 
 def index(request):
 	print "index"
@@ -17,11 +20,27 @@ def detail(request, detail_id):
 	detail_display = get_object_or_404(Listing, pk = detail_id)
 	return render(request, 'topmenu/detail.html', {'apples': detail_display})
 
+
+# receieve sms method
 @csrf_exempt
 @require_POST
 def plivo_endpoint(request):
-	print request.POST['From']
+	source = request.POST['From']
+	destination = request.POST['To']
+	messageuuid = request.POST['MessageUUID']
+	message_content = request.POST['Text']
+	message_time = datetime.now()
 	return HttpResponse()
+
+def send_message(src, dest, text):
+	pass
+
+
+
+
+
+
+
 
 
 
