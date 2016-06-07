1047c9ab37382cfa975d6a209224a35ea664c16b
alando46 <music2177@gmail.com>
2016-06-07 06:23:54 -0700

    adding postgres back in

--- smscl/settings/base.py
+++ smscl/settings/base.py
@@ -56,8 +56,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alando'
+        'USER': 'postgres'
+        'PASSWORD': 'password'
+        'HOST': 'localhost'
+        'PORT': '5432'),
     }
 }
 
