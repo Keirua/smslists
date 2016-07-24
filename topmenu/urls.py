3d01101aeb490eab51dafbb2f54f68e7215d6009
alando46 <music2177@gmail.com>
2016-07-23 19:27:12 -0700

    minor url config edits

--- topmenu/urls.py
+++ topmenu/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
 	url(r'^/listings/(?P<category>\w+)/', views.listings, name='listings'),
 
 	# NOTE: category doesn't matter/superflous here, since listings have unique id's across categories anyway
-	url(r'^/listings/(?P<category>\w+)/(?P<id>\d+)/', views.listing_detail, name='listing_detail'),
-	url(r'^/post_subject_request/(?P<category>\w+)/', views.post_subject_request, name='post_subject_request'),
-	url(r'^/post_description_request/(?P<category>\w+)/', views.post_description_request, name='post_description_request'),
-	url(r'^/post_review/(?P<category>\w+)/', views.post_review, name='post_review'),
-	url(r'^/post_commit/(?P<category>,\w+)/', views.post_commit, name='post_commit'),
+	url(r'^listings/(?P<category>\w+)/(?P<id>\d+)/', views.listing_detail, name='listing_detail'),
+	url(r'^post_subject_request/(?P<category>\w+)/', views.post_subject_request, name='post_subject_request'),
+	url(r'^post_description_request/(?P<category>\w+)/', views.post_description_request, name='post_description_request'),
+	url(r'^post_review/(?P<category>\w+)/', views.post_review, name='post_review'),
+	url(r'^post_commit/(?P<category>,\w+)/', views.post_commit, name='post_commit'),
 	]
