e42c4137fbcf1a55d30cabd32a1d9c4063b0e596
Laszlo Marai <atleta@atleta.hu>
2016-10-19 02:02:54 +0200

    Extended ActiveUrlsBuilderNode to handle variables in the query params section

--- topmenu/templatetags/link_handling.py
+++ topmenu/templatetags/link_handling.py
@@ -1,7 +1,7 @@
 import re
 
 from django import template
-from django.template.base import Token
+from django.template.base import Token, VariableDoesNotExist
 from django.template.defaulttags import url, URLNode
 
 register = template.Library()
@@ -19,7 +19,16 @@ class ActiveUrlsBuilderNode(template.Node):
 	def render(self, context):
 		print 'ActiveUrlsBuilderNode context = %s' % context
 
-		url = self.url_node.render(context) + '' if not self.query_params else '?%s' % '&'.join(self.query_params)
+		query_params = []
+		for param in self.query_params:
+			name, value = param.split('=')
+			try:
+				value = template.Variable(value).resolve(context)
+			except VariableDoesNotExist:
+				pass
+			query_params.append('%s=%s' % (name, value))
+
+		url = self.url_node.render(context) + '' if not query_params else '?%s' % '&'.join(query_params)
 		request = context['request']
 		print 'url = %s' % url
 
@@ -39,13 +48,14 @@ def active_urls_builder(parser, token):
 
 	if LINK_RX.match(bits[1]):
 		link_id = bits[1]
-		bits = [bits[0]]+bits[2:]
+		bits = [bits[0]] + bits[2:]
 
 	if QUERY_PARAM_SEPARATOR in bits:
 		query_params_start = bits.index(QUERY_PARAM_SEPARATOR)
 		query_params = bits[query_params_start + 1:]
 		bits = bits[:query_params_start]
 
+	print '!!', token.contents, bits, ' '.join(bits)
 	url_node = url(parser, Token(token.token_type, ' '.join(bits), token.position, token.lineno))
 
 	return ActiveUrlsBuilderNode(url_node, link_id, query_params)
